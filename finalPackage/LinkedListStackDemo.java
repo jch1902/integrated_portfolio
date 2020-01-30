be6a9428c532229e103742fa73204ee90a3169a5
Jasper <jasper.d39c@gmail.com>
2020-01-29 20:58:48 -0800

    added cool gui

--- finalPackage/LinkedListStackDemo.java
+++ finalPackage/LinkedListStackDemo.java
@@ -4,29 +4,44 @@ import java.util.*;
 
 public class LinkedListStackDemo {
 	private static LinkedList<String> newList;
+	private static ArrayList printedString;
 	public LinkedListStackDemo(){
 		newList = new LinkedList<String>();
 	}
 	public static void createList() {
 		String[] wishList = {"Bike","Airpods","Macbook","Microphone"};
 		int x = 0;
+		printedString = new ArrayList();
+		
 		System.out.println("Wish List: " + newList);
+		printedString.add("Wish List: " + newList);
 		for(int i = 0; i < wishList.length; i++) {
 			newList.push(wishList[x]);
+			
 			System.out.println("Wish List: (Added " + wishList[x] + ") "+ newList);
+			printedString.add("Wish List: (Added " + wishList[x] + ") "+ newList);
+			
 			x++;
 		}
+		
 		System.out.println("Wish list is full. Let's go buy stuff and remove it from the list.");
+		printedString.add("Wish list is full. Let's go buy stuff and remove it from the list.");
+		
 		x = 0;
 		for(int i = 0; i < wishList.length; i++) {
 			newList.removeLast();
 			if(newList != null) {
 				System.out.println("Wish List: (Got " + wishList[x] + ") " + newList);
+				printedString.add("Wish List: (Got " + wishList[x] + ") " + newList);
 			}else {
 				System.out.println("Wish List: Empty ");
 			}
 			x++;
 		}
+	}
+	public String returnArrayList() {
+		return printedString.get(0).toString() + "\n" + printedString.get(1).toString() + "\n"
+				+ printedString.get(2).toString() + "\n"+ "\n";
 	}
 	
 }
--- finalPackage/LinkedListUI.java
+++ finalPackage/LinkedListUI.java
@@ -2,10 +2,16 @@ package finalPackage;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class LinkedListUI extends JFrame {
 
@@ -26,9 +32,6 @@ public class LinkedListUI extends JFrame {
 				System.out.println("---Queue---");
 				LinkedListQueueDemo newQueueList = new LinkedListQueueDemo();
 				newQueueList.createList();
-				System.out.println("---Stack---");
-				LinkedListStackDemo newStackedList = new LinkedListStackDemo();
-				newStackedList.createList();
 			}
 		});
 	}
@@ -43,6 +46,36 @@ public class LinkedListUI extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JLabel lblList = new JLabel("Click the buttons on the side to make a list display");
+		contentPane.add(lblList, BorderLayout.CENTER);
+		
+		JButton btnStack = new JButton("Stacked List");
+		btnStack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("---Stack---");
+				LinkedListStackDemo newStackedList = new LinkedListStackDemo();
+				newStackedList.createList();
+				lblList.setText("<html>" + newStackedList.returnArrayList()
+															.replaceAll("<","&lt;")
+															.replaceAll(">", "&gt;")
+															.replaceAll("\n", "<br/>")
+															+ "</html>");
+			}
+		});
+		contentPane.add(btnStack, BorderLayout.WEST);
+		
+		JButton btnQueue = new JButton("Queued List");
+		btnQueue.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("---Queue---");
+				LinkedListQueueDemo newQueueList = new LinkedListQueueDemo();
+				newQueueList.createList();
+			}
+		});
+		contentPane.add(btnQueue, BorderLayout.EAST);
+		
+		
 	}
 }
 
