3628969336175d2f7003ce58e371051fb8ce3af6
Jasper <jasper.d39c@gmail.com>
2020-01-29 22:06:11 -0800

    added colors

--- finalPackage/LinkedListUI.java
+++ finalPackage/LinkedListUI.java
@@ -45,6 +45,7 @@ public class LinkedListUI extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 204));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
